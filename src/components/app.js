77e923cff3ad1f7748ba94213d9ede7268de1434
brian <brian.p.coons@gmail.com>
2019-12-18 14:20:20 -0700

    class vs functional components - added portfolio-items

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
-import PortfolioContainer from "./portfolio-container"
+import PortfolioContainer from "./portfolio/portfolio-container"
 
 export default class App extends Component {
   render() {
@@ -9,8 +9,8 @@ export default class App extends Component {
       <div className='app'>
         <h1>Brian's Portfolio</h1>
         <h2>Rayn is the wrost</h2>
-        <PortfolioContainer />
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />
       </div>
     );
   }
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,16 @@
 import React, {Component} from "react"
 
+import PortfolioItem from "./portfolio-item"
+
 export default class PortfolioContainer extends Component {
+    // State requires Class based - if we are getting and changing data(forms and such)
+    // Functional is gd for rendering
     render(){
         return (
             <div>
                 <h2>Portfolio Items still go here</h2>
+
+                <PortfolioItem/>
             </div>
         )
     }
