3890a676f119bf17bbc399848f04a66506dbb4fc
brian <brian.p.coons@gmail.com>
2020-01-09 15:27:42 -0700

    added login page and api verification

---
+++ src/components/auth/login.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Login extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      email: "",
+      password: "",
+      errorText: ""
+    }
+
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: event.target.value,
+      errorText: ""
+    })
+  }
+
+  handleSubmit = e => {
+    axios.post("https://api.devcamp.space/sessions", 
+    {
+      client: {
+        email: this.state.email,
+        password: this.state.password
+      }
+    },
+    {withCredentials: true}
+    ).then(response => {
+      if(response.data.status === 'created'){
+        console.log("you did it!")
+      }else {
+        this.setState({
+          errorText: "Email or Password Incorrect"
+        })
+      }
+    }).catch(error => {
+      this.setState({
+        errorText: "An Error has Occurred"
+      })
+    })
+    e.preventDefault()
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+        <div>{this.state.errorText}</div>
+
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="email" 
+            name="email"
+            placeholder="Your Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input 
+            type="password" 
+            name="password"
+            placeholder="Your Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ src/components/pages/auth.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import loginImg from "../../../static/assets/images/auth/login.jpg"
+import Login from "../auth/login.js"
+
+export default class Auth extends Component {
+    render() {
+        return (
+            <div className="auth-page-wrapper">
+                <div 
+                    className="left-column"
+                    style={{ backgroundImage: `url(${loginImg})`}}
+                />
+
+                <div className="right-column">
+                    <Login />
+                </div>
+            </div>
+        )
+    }
+}
